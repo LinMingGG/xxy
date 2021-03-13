5cc04b4db51e8cfe962a8f5414848c23f6352d2c
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2021-03-13 16:39:28 +0800

    Create xxy.py

---
+++ xxy.py
@@ -0,0 +1,54 @@
+
+import requests
+import json
+import time 
+import os
+#习讯云签到脚本
+   
+data={'account':'1833010512',#账号
+      'app_id':'cn.vanber.xixunyun.saas',
+      'app_version':'4.9.9',
+      'key':'',
+      'model':'huawei tag-tl00',
+      'password':'qj200009.',#密码
+      'platform':'2',
+      'registration_id':'1a0018970a242ca8039',
+      'request_source':'3',
+      'school_id':'1743',#学校代码
+      'system':'5.1.1',
+      'uuid':'00:81:24:d9:fc:da'}
+login_header={
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': '227',
+        'Host': 'api.xixunyun.com',
+        'Connection': 'Keep-Alive',
+        'User-Agent': 'okhttp/3.8.1',
+
+}
+login_url=' https://api.xixunyun.com//login/api?from=app&version=4.9.9&platform=android&entrance_year=0&graduate_year=0'
+request=requests.post(url=login_url,headers=login_header,data=data)
+login_data=json.loads(request.text)#登陆成功后返回的信息
+token=login_data['data']['token']
+time.sleep(1)
+#经纬度地址
+latitude='23.870538'#维度
+longitude='120.521263'#经度
+print(login_data)
+sign_url='https://api.xixunyun.com/signin_rsa?token='+token+'&from=app&version=4.9.9&platform=android&entrance_year=0&graduate_year=0 '
+sign_data={'address':'',#签到地址
+           'address_name':'',#签到地点名称
+           'change_sign_resource':'1',
+           'comment':'',
+           'latitude':"PcsiE2OsHB5/8tdtamFezKcg9jYp4lKOryX8mG68mE32nVFwh1BNzaJgDIaUK0QOnz7iGDgOcyGMwbB85zJT7wf3Dczy5O9z3/mTBOH9zOFhxKhUSUqlpKdfG5+ZV8UJF6evTcYci5YPIpqGI6uifnQmyB/X86bcNGqe1f2i0WU=",
+           'longitude':"fS0dMPGfCMjpBmLZgJKV6GXjlC/PD8ZaVlsxSsXopLOPkctgbRuNfGThWBXdjuOY0ESvvWg+l6oYrhMsiHvrTrpJKyy95bMhDW83SNbLrvHQcvdlo5I+ApSYTBNRxvRHqDlSOifwRUdweCBKgrSs26YLP0JWZPwAK54uwNruN3U=",
+           'remark':'0',
+    
+    }
+sign_request=requests.post(url=sign_url,data=sign_data,headers=login_header)
+sign=json.loads(sign_request.text)
+print(sign)
+
+
+os.system("pause")
+
+
