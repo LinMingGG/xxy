e68501ad9a3617144c00db200e87562c63801e67
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2021-03-13 23:05:57 +0800

    Update xxy.py

--- xxy.py
+++ xxy.py
@@ -52,18 +52,15 @@ sign_data={'address':os.environ["ADDRESS"],#签到地址
            'remark':'0',
     
     }
-#sign_request=requests.post(url=sign_url,data=sign_data,headers=login_header)
-#sign=json.loads(sign_request.text)
-#print(sign.json)
+sign_request=requests.post(url=sign_url,data=sign_data,headers=login_header)
+sign=json.dump(sign_request.text)
+print(sign)
 
-
-response2 = requests.post(url=sign_url,data=sign_data,headers=login_header)
-print(response2.json())
                                      
 SCKEY=os.environ["SCKEY"]
 if len(SCKEY) >= 1:
   url = 'https://sc.ftqq.com/'+SCKEY+'.send'
-  requests.post(url, data={"text": "习讯云签到提醒", "desp": response2.json()})
+  requests.post(url, data={"text": "习讯云签到提醒", "desp": sign})
 os.system("pause")
 
 
