2faa2a544020c9f354f6cca9ab39ba430606e915
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2021-03-14 10:13:08 +0800

    Update xxy.py

--- xxy.py
+++ xxy.py
@@ -47,7 +47,7 @@ print(login_data)
 sign_url='https://api.xixunyun.com/signin_rsa?token='+token+'&from=app&version=4.1.5&platform=android&entrance_year=0&graduate_year=0 '
 sign_data={'address':'',#签到地址
            'address_name':address,#签到地点名称
-           'change_sign_resource':'1',
+           'change_sign_resource':'0',
            'comment':'',
            'latitude':"PcsiE2OsHB5/8tdtamFezKcg9jYp4lKOryX8mG68mE32nVFwh1BNzaJgDIaUK0QOnz7iGDgOcyGMwbB85zJT7wf3Dczy5O9z3/mTBOH9zOFhxKhUSUqlpKdfG5+ZV8UJF6evTcYci5YPIpqGI6uifnQmyB/X86bcNGqe1f2i0WU=",
            'longitude':"fS0dMPGfCMjpBmLZgJKV6GXjlC/PD8ZaVlsxSsXopLOPkctgbRuNfGThWBXdjuOY0ESvvWg+l6oYrhMsiHvrTrpJKyy95bMhDW83SNbLrvHQcvdlo5I+ApSYTBNRxvRHqDlSOifwRUdweCBKgrSs26YLP0JWZPwAK54uwNruN3U=",
